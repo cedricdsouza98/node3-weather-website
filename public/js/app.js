04a7d10fdc90b1109282120b6ff20f4e98b33fb0
cedricdsouza98 <cedricdsouza98@gmail.com>
2021-06-24 14:22:06 +0530

    removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,8 @@
-console.log('client side javascript')
-
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
-
-
-
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
